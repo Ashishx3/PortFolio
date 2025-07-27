bba37a1ab5ef9f63063fee451d610f7a92932c52
AshishX3 <ashishsinghyt111@gmail.com>
2025-07-27 15:34:56 +0530

    Portfolio Pushed

--- components/Smoothcursor.jsx
+++ components/Smoothcursor.jsx
@@ -72,7 +72,7 @@ export default function Smoothcursor() {
   return (
     <div className="pointer-events-none fixed inset-0 z-50">
       <div
-        className="absolute rounded-full dark:bg-white bg-black "
+        className="absolute rounded-full dark:bg-white bg-white "
         style={{
           width: '8px',
           height: '8px',
