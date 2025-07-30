e04c19dde0fc2a8acb820a5a4093a11182cb7d0e
AshishX3 <ashishsinghyt111@gmail.com>
2025-07-30 20:03:58 +0530

    Portfolio Pushed

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -44,16 +44,17 @@ Frontend built with React and deployed on Vercel; backend built with Express and
   githublink="https://github.com/Ashishx3/ExamSavior"
 />
 
-        <Card
-        imagelink="/projectsimage/codeverse.png"
-        langs={["React.js","Tailwind","Router","Sql"]}
-          title="Code Verse"
-          content="A notes platform with TTS, last-year papers, and key questions for quick exam prep.
-Built with Express (API + Routes), EJS, HTML/CSS/JS, and PostgreSQL.
-Includes user authentication for login and signup to personalize the experience."
-          livelink="https://examsavior.net"
-          githublink="https://github.com/Ashishx3/ExamSavior"
-        />
+    <Card
+  imagelink="/projectsimage/codeverse.png"
+  langs={["React.js", "Tailwind", "React Router", "SQL"]}
+  title="Code Verse"
+  content="A robust and scalable platform being built with React.js and Tailwind CSS. 
+It utilizes React Router for seamless navigation and SQL for structured data handling. 
+Currently under development with a focus on performance, responsiveness, and clean UI/UX."
+  livelink="https://examsavior.net"
+  githublink="https://github.com/Ashishx3/ExamSavior"
+/>
+
         <Card
   imagelink="/projectsimage/tts.png"
   langs={["HTML", "CSS", "JS","Python"]}
@@ -62,16 +63,17 @@ Includes user authentication for login and signup to personalize the experience.
   livelink="https://textospeech-one.vercel.app"
   githublink="https://github.com/Ashishx3/Text-to-speech"
 />
-        <Card
-        imagelink="/projectsimage/portfolio.png"
-        langs={["Next JS","Tailwind", "Framer","AOS","Cursify"]}
-          title="Exam Savior"
-          content="A notes platform with TTS, last-year papers, and key questions for quick exam prep.
-Built with Express (API + Routes), EJS, HTML/CSS/JS, and PostgreSQL.
-Includes user authentication for login and signup to personalize the experience."
-          livelink="https://ashishsinghportfolio.vercel.app"
-          githublink="https://github.com/Ashishx3/ExamSavior"
-        />
+      <Card
+  imagelink="/projectsimage/portfolio.png"
+  langs={["Next.js", "Tailwind CSS", "FramerMotion", "AOS", "Cursify"]}
+  title="My Portfolio"
+  content="A modern, responsive personal portfolio showcasing my projects, skills, and resume. 
+Built using Next.js, styled with Tailwind CSS, and enhanced with smooth animations using Framer Motion and AOS. 
+Features email integration and dynamic project components to highlight work efficiently."
+  livelink="https://ashishsinghportfolio.vercel.app"
+  githublink="https://github.com/Ashishx3/PortFolio"
+/>
+
       </div>
     </div>
   );
